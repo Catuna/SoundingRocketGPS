9f0e49cc7acdc1c2d14cc943d3233d2b133285b9
Joakim R. Andersen <jra93@hotmail.com>
2017-02-15 21:44:40 +0100

    Works. For some reason it starts to read the wrong byte value. This is solved by just disregarding the 2500 messages.

---
+++ serial_monitoring.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import serial
+import threading
+
+COMPORT = "COM4" #Update accordingly!
+BAUDRATE = 115200
+
+def monitor():
+    print ("Start serial monitoring")
+    while(1):
+        #Try/excepts loop is in case we start this program before the arduino-program which sends to the given port.
+        try:
+            ser = serial.Serial(COMPORT, BAUDRATE, timeout = 0)
+            break;
+        except:
+            continue;
+    readJunk = 0;
+    while(1):
+
+        msgAsByte = ser.read()
+        if (msgAsByte != "" and msgAsByte !='' and msgAsByte != "\n" and msgAsByte != b"" and msgAsByte !=b'' and msgAsByte != b"\n"):
+            msgAsUint8 =  int.from_bytes(msgAsByte, byteorder='little');
+            readJunk +=1;
+            if readJunk >= 2500: ##Hack as hell. Hver gang man begynner aa lytte (uansett hvem som starter), saa leser den feil i starten.
+                if(msgAsUint8 != 192):
+                    print("baaad");
+                with open("meldingFraNyttelast.txt", "a") as text_file:
+                    text_file.write(str(msgAsUint8)+"\n")
+                    
+    print ("Stop serial monitoring")
+
+monitor()
+
